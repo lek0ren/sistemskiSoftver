f01b0211e07fe56d40b480e394536914e4580273
Veljko Povovic <lek0ren@github.com>
2020-05-15 16:59:45 +0200

    krenuo parser

---
+++ kod.s
@@ -0,0 +1,29 @@
+.global a
+.extern b
+
+.text
+.equ a , b
+mov eax,b
+iret
+halt
+push $56
+pop simbol
+pop %r5
+push (%r7)
+pop %r5l
+pop %r5h
+push (%r2)
+jmp *%r2
+call labela
+jmp 54
+jeq *54
+jne *(%r6)
+jgt *simbol(%r2)
+pushw %r1
+
+           c: .byte 2
+
+.data
+a: .long 8
+
+.end
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,75 @@
 #include <iostream>
+#include <fstream>
+#include <regex>
+#include <string.h>
+#include <stdio.h>
 
-using namespace std;
+std::regex label("[/w]+:");
 
-int main(){
-    cout<< "zdravo svete!"<<endl;
+class Token
+{
+private:
+    std::string token;
+
+    enum type {
+        INSTR = 1,
+        LABEL,
+        LITERAL,
+        SIMBOL,
+        REGVAL,
+        MEMREGVAL,
+        MEMVALWITHREG
+    };
+
+public:
+    Token(std::string token) : token(token) {}
+
+    Token(char *string) {
+        token = std::string(string);
+    }
+
+    std::string getToken()
+    {
+        return token;
+    }
+};
+
+char *stringToCharArray(std::string string, char *array)
+{
+    strcpy(array, string.c_str());
+    return array;
+}
+
+
+
+int main()
+{
+    std::ifstream in("kod.s");
+
+    if (!in)
+    {
+        std::cout << "Cannot open input file.\n";
+    }
+
+    std::string line;
+
+    char *array = (char *)std::malloc(255 * sizeof(char));
+
+    while (std::getline(in, line))
+    {
+        //replace starting offset with nothing
+        //std::cout << line << std::endl;
+        char *token = strtok(stringToCharArray(line, array), " ,\n\t");
+        std::vector<Token> instruction;
+
+        while (token)
+        {
+            Token t(token);
+            instruction.push_back(t);
+            std::cout << t.getToken() << std::endl;
+            token = strtok(nullptr, " ,\n\t");
+        }
+    }
+    in.close();
     return 0;
 }
